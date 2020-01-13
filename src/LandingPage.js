f110a3d3fbfbabb6679d14ef22bfada577cc1e7a
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-13 17:09:47 -0500

    MVP Implementation

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,44 +4,28 @@ import './css/LandingPage.css'
 
 export default class LandingPage extends React.Component {
   render() {
+    let resources = this.state.resources;
     return (
       <section class="LandingContainer">
         <div><h1 id="HeaderText">Blog Name</h1>
           <p>Blog description & explanation</p>
           <div>
-          <Link to="/sign-up"><button>Sign Up</button></Link>
-          <Link to="/login"><button>Login</button></Link>
+            <Link to="/sign-up"><button>Sign Up</button></Link>
+            <Link to="/login"><button>Login</button></Link>
           </div>
           <section id="resourceList">
-          <div id="lp-resource-component-container" >
-            <div id="resource" ><h1>Resource Type 1</h1>
-            <div id="componentImage"></div>
-            <div id="componentDescription">
-                                <p>Component Description</p>
-            </div>
-            </div>
-        
-            <div id="resource" ><h1>Resource Type 2</h1>
-            <div id="componentImage"></div>
-            <div id="componentDescription">
-                                <p>Component Description</p>
-            </div>
-        </div>
-    
-            <div id="resource" ><h1>Resource Type 3</h1>
-            <div id="componentImage"></div>
-            <div id="componentDescription">
-                                <p>Component Description</p>
-            </div>
-        </div>
-    
-            <div id="resource" ><h1>Resource Type 4</h1>
-            <div id="componentImage"></div>
-            <div id="componentDescription">
-                                <p>Component Description</p>
-            </div>
-        </div>
-    </div>
+            <div id="lp-resource-component-container" >
+              {resources.slice(0,3).map(function (resource) {
+                return             <section id="resource">
+                <div id="resourceImage"><img id="previewImage" alt={"Headline Image for " + resource.headline} src={resource.image} /></div>
+                <div id="resourceText">
+                  <h3 id="resourceText">{resource.name}</h3>
+                  <p id="resourceText">{resource.summary}</p>
+                  <Link to={resource.url}><button>Go to Resource</button></Link>
+                </div>
+              </section>})
+              }
+          </div>
           </section>
         </div>
       </section>
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,25 +1,62 @@
 import React from 'react'
 import './css/LoginForm.css'
 import { Link } from 'react-router-dom';
+import UserService from './services/user-service'
 
 export default class LoginForm extends React.Component {
-    render() {
-        return (
-            <section class="LandingContainer">
-                <div class="LoginForm" > <h1 id="HeaderText"> Login Form Header </h1>
-                <div>
-                    <label for="Username" > Username </label>
-                    <input type="text" />
-                    <label for="Password" > Password </label>
-                    <input type="text" />
-                    <br />
-                    </div>
-                    <div>
-                    <Link to="/dashboard"><button>Submit</button></Link>
-                    <Link to="/sign-up"><button>Sign Up</button></Link>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  state = { error: null }
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    this.setState({ error: null })
+    const { username, password } = ev.target
+
+    UserService.postLogin({
+      username: username.value,
+      password: password.value,
+    })
+      .then(res => {
+        username.value = ''
+        password.value = ''
+        this.props.onLoginSuccess(res.username)
+        this.props.history.push('/dashboard')
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+
+      })
+  }
+
+
+  render() {
+
+
+    return (
+      <section class="LandingContainer">
+        <div class="LoginForm" > 
+        <div class="loginFormHeader">
+          <h1 class="lpHeaderText"> Welcome, Please Sign In! </h1>
+         <p> Guests, please login with 
+           Username: GenBlanc and Password: Password34!</p>
+         </div>
+                    <form onSubmit={this.handleSubmitJwtAuth} className='RegistrationForm' >
+            <div class='formLine'>
+              <label for="username" > Username </label>
+              <input type="text" name="username" />
+            </div>
+            <div class='formLine'>
+              <label for="password" > Password </label>
+              <input type="password" name="password" />
+              <br />
+            </div>
+            <div class='signupButtons'>
+              <button class="lpButton" >Submit</button>
+              <Link to="/sign-up"><button class="lpButton" >Sign Up</button></Link>
+              <div>{this.state.error}</div>
+            </div>
+          </form>
+        </div>
+      </section>
+    );
+  }
 }
