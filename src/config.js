ed324f97406385ee4842be7b99384e45e57982de
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-13 11:44:21 -0500

    MVP Implementation

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 5432,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||  'https://web-dev-toolkit-api.herokuapp.com/api',
+    API_BASE_URL: 'https://web-dev-toolkit-api.herokuapp.com/api',
+    DB_URL: process.env.DB_URL || "postgres://bjdtgbaxupkxmh:2b6cacf62d5382f61d8f543d7d320bbb75a9e010e98b37adc725f5d4182e46b6@ec2-174-129-33-2.compute-1.amazonaws.com:5432/d7oq5p5pdkm8fk",
     TOKEN_KEY: 'WebDevToolkit-auth-token',
   }
