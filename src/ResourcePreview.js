6c01af22922055f962f884bece3890847a200367
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-13 12:09:53 -0500

    MVP Implementation

--- src/ResourcePreview.js
+++ src/ResourcePreview.js
@@ -8,9 +8,10 @@ export default class ResourcePreview extends React.Component {
 
     render(props) {
         // id= {this.props.resource.id};
+        let resource = this.props.resource;
             return(
             <section id="resource">
-                <div id="resourceImage">{this.props.resource.image}</div>
+                <div id="resourceImage"><img id="previewImage" alt={"Headline Image for " + resource.headline} src={resource.image} /></div>
                 <div id="resourceText">
                     <h3 id="resourceText">{this.props.resource.name}</h3>
                     <p id="resourceText">{this.props.resource.summary}</p>
