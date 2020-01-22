29c7f5f6f001a881c7211116fd932bb71798d01f
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-21 23:52:16 -0500

    Landing Page & Resource View Page Adjustment

--- src/ResourceTypeView.js
+++ src/ResourceTypeView.js
@@ -15,6 +15,18 @@ handleURLButton = (resourceURL) => {
         let resourceType = this.props.resourceType;
 /*         let typeResources= this.props.typeResources
         let currentResource = resources.filter(resource = resource.id == resourceID)  */
+        if(resourceType !== "Tutorials" || "Courses" || "Videos" || "Utilities"){ return (
+            <div id="resource-Full">
+                   <div id="resourceText">
+                    <h3 id="resourceText">{resource.headline}</h3>
+                    <p id="resourceText">{resource.summary}</p>
+                </div>
+                <div id="resourceButtons">
+           <button onClick={this.handleURLButton.bind(this, resource.url)} >Go to Resource</button><button onClick={this.props.handleFavoriteButton}>Favorite Button</button>
+                </div>
+            </div>
+    );}
+        else{
         return (
                 <div id="resource-Full">
                     <div id="resourceImage"><img id="previewImage" alt={"Headline Image for " + resource.headline} src={"/images/" + resourceType + ".svg"} /></div>
@@ -26,7 +38,7 @@ handleURLButton = (resourceURL) => {
                <button onClick={this.handleURLButton.bind(this, resource.url)} >Go to Resource</button><button onClick={this.props.handleFavoriteButton}>Favorite Button</button>
                     </div>
                 </div>
-        );
+        );}
     }
 }
 
